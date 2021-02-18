137f3e82f582f4cfdb5327086d143135a3cb53ea
Augusto <augusto.scher@resultadosdigitais.com.br>
2021-02-17 23:28:18 -0300

    Styling HomePage

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Image de um atomo e React Avançado escrito ao lado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript. ReactJS, NextJS e StyledComponents
+    </S.Description>
+    <S.Ilustration
+      src="/img/hero-illustration.svg"
+      alt="Desenvolvedor sentado na cadeira de frente para a tela"
+    />
+  </S.Wrapper>
 )
 
 export default Main
